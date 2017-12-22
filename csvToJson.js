9c581936b0776bc61593141789c9a5a2127bb375
fangstar <fangstar@fangstars-mac.local>
2017-12-21 16:40:11 -0800

    commit for lab.

---
+++ csvToJson.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const csvFilePath='./customer-data.csv';
+const csv = require('csvtojson');
+
+const outputJsonFile = 'customer-data.json';
+
+let buffer = [];
+
+csv()
+.fromFile(csvFilePath)
+.on('json',(jsonObj) => {
+  buffer.push(jsonObj);
+})
+.on('done', (error) => {
+  console.log('end')
+  fs.writeFileSync(outputJsonFile, JSON.stringify(buffer));
+})
